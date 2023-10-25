5c6819dd111d441b6d0278bd503d67f53dcb3746
samuel cooper <cooper.src261@gmail.com>
2023-10-25 13:13:02 -0700

    Added named operator is

--- intermediate/namedparams.dart
+++ intermediate/namedparams.dart
@@ -1,1 +1,21 @@
+//Named operators are used for more complex operations than the usual symbolic operators.
 
+// ignore_for_file: unnecessary_type_check
+
+//is will check if something is of type
+int one = 1;
+var intCheck = one is int;
+var dubCheck = one is double;
+var strCheck = one is String;
+var boolCheck = one is bool;
+
+void main() {
+  print('''
+Variable 'one' Results
+-----------------------
+Integer: $intCheck
+Double : $dubCheck
+String : $strCheck
+Boolean: $boolCheck
+''');
+}
