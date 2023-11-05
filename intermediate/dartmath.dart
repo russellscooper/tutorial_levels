dfd4ec466a6dfa30d64720f7f2edad40a4485eb8
samuel cooper <cooper.src261@gmail.com>
2023-11-04 20:32:03 -0700

    more math

--- intermediate/dartmath.dart
+++ intermediate/dartmath.dart
@@ -1,7 +1,17 @@
 //Working with numbers using dart
 import 'dart:math';
 
+//Class numbers called num with generic type.
+class num<T> {
+  T value;
+  num(this.value);
+  T gerValue() {
+    return value;
+  }
+}
+
 void main() {
+  //Sample geometry problem
   double r = 5.0;
   double circumference = 2 * pi * r;
   double area = pi * pow(r, 2);
@@ -12,4 +22,13 @@ void main() {
   Circumference: $circumference
   Area: $area
 ''');
+
+  //There are many builtin ways to do math in dart
+  int largest = max(100, 101);
+  print(largest);
+
+  //also built in constants
+  double A = 10.0;
+  double sqrt2TimesA = A * sqrt2;
+  print(sqrt2TimesA);
 }
