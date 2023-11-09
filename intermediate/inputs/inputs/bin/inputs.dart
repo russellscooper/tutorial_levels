bd11286f4c3685b60706f4e926dbf3e87cc6f4fc
samuel cooper <cooper.src261@gmail.com>
2023-11-09 09:50:52 -0800

    added inputs

---
+++ intermediate/inputs/inputs/bin/inputs.dart
@@ -0,0 +1,5 @@
+import 'package:inputs/inputs.dart' as inputs;
+
+void main(List<String> arguments) {
+  print('Hello world: ${inputs.calculate()}!');
+}
---
+++ intermediate/inputs/inputs/test/inputs_test.dart
@@ -0,0 +1,8 @@
+import 'package:inputs/inputs.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
