7726887b0ce3ee49d307226cf3d45b36d7b3255b
samuel cooper <cooper.src261@gmail.com>
2023-11-10 19:14:31 -0800

    Set up cli app skeleton

--- intermediate/inputs/terminal_inputs/bin/terminal_inputs.dart
+++ intermediate/inputs/terminal_inputs/bin/terminal_inputs.dart
@@ -1,17 +1,37 @@
+//This is a skeleton for console apps.
+//As with any good skeletons, it can be cloned to other projects and the internals attatched.
+
 import 'package:dart_console/dart_console.dart';
 
 int main() {
   final console = Console();
 
+  console.setBackgroundColor(ConsoleColor.blue);
+  console.setForegroundColor(ConsoleColor.white);
   console.clearScreen();
   console.resetCursorPosition();
-
+  console.writeLine(
+    '''
+ #######################################################################################
+ #      WELCOME TO YOUR NEW CLI APP! Change and modify this file to add content!       #
+ #######################################################################################
+  ''',
+    TextAlignment.left,
+  );
+  //Create a body
   console.writeLine(
     'Console size is ${console.windowWidth} cols and ${console.windowHeight} rows.',
     TextAlignment.center,
   );
-
-  console.writeLine();
-
+  //Create a footer
+  console.writeLine(
+    '''
+  Thank You for Using Our Tech! 
+                        
+                        Copyright \u00A9 [Your Name] 2023
+    ''',
+    TextAlignment.left,
+  );
+  console.resetColorAttributes();
   return 0;
 }
